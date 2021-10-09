db3739de39b569a2f75738352311f530a768f7eb
tawsifhye <tawsif.lucse@gmail.com>
2021-10-09 16:15:00 +0600

    fixed bg image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,15 +7,15 @@ const Header = () => {
     return (
         <div className=''>
             <nav className="navbar navbar-expand-lg navbar-light self-navbar d-flex justify-content-end align-items-center self-nav">
-                <div className="container ms-0 ps-3">
+                <div className="container w-50 ms-0 ps-3">
                     <Link to='/home'>
                         <img src={logo} alt="" width="111" height="92"/>
                     </Link>
                 </div>
-                <div className="container-fluid d-flex">
-                    <button className="navbar-toggler navbar-toggler-right bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler navbar-toggler-right bg-light me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+                <div className="container-fluid d-flex">
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto pe-lg-5 mb-2 mb-lg-0 fw-bold fs-5">
                             <li className="nav-item">
