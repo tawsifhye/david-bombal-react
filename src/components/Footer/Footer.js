006b16d05fa0013e9b52aef38824dacc3463f7d6
tawsifhye <tawsif.lucse@gmail.com>
2021-10-31 19:28:31 +0600

    footer text edited

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,10 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer>
-            <div className='footer p-5 align-bottom'>
-                <p className='text-white text-center'>Copyright ©2021 All rights reserved DavidBombal.com
+            <div className='footer p-5 text-white text-center'>
+                <p>Copyright ©2021 All rights reserved DavidBombal.com
                 </p>
+                <small>This website is developed for learning purpose. Pictures and other information are collected from https://davidbombal.com/ and other websites </small>
             </div>
         </footer>
     );
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './NotFound.css'
 const NotFound = () => {
     return (
-    <div className='container-fluid h-100'>
+    <div className=''>
         <img className='w-100' src="https://image.freepik.com/free-vector/error-404-concept-landing-page_52683-20650.jpg" alt="" />
     </div>                
     );
